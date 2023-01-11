9cbc2ba2ccce51939225d6e48ebc17e3b8fac931
Magdalena Reljin <reljin.ra82.2018@uns.ac.rs>
2023-01-11 11:34:45 +0100

    uploading images

--- sport-club/sport-club-frontend/src/MainPage/UserProfileAndOptions/UserProfileAndOptions.tsx
+++ sport-club/sport-club-frontend/src/MainPage/UserProfileAndOptions/UserProfileAndOptions.tsx
@@ -1,10 +1,16 @@
+import { useEffect } from "react";
 import { ActivityStatus, UserImage, UserProfileAndOptionsFrame } from "./styled-user-profile-and-options"
-
+import '../../images/proba.jpg'
 const UserProfileAndOptions = () => {
    
+    useEffect(()=>{
+      
+    },[])
+
+
     return (
        <UserProfileAndOptionsFrame>
-            <UserImage url="" ></UserImage>
+            <UserImage  src={require('../../images/proba.jpg')} ></UserImage>
             <ActivityStatus></ActivityStatus>
        </UserProfileAndOptionsFrame>
     );
--- sport-club/sport-club-frontend/src/MainPage/UserProfileAndOptions/styled-user-profile-and-options.ts
+++ sport-club/sport-club-frontend/src/MainPage/UserProfileAndOptions/styled-user-profile-and-options.ts
@@ -1,13 +1,14 @@
+
 import styled from "styled-components";
 import { colors } from "../colors";
+
 export const UserProfileAndOptionsFrame = styled.div`
     margin-left: 483px;
     padding-top: 13px;
     position: relative;
 `;
-export const UserImage = styled.div<{url: string}>`
+export const UserImage = styled.img`
     position: absolute;
-    background-image: '../../images/fudbaler.jpg';// ${props => props.url}; 
     border: 2px solid ${colors.gallery};
     width: 49px;
     height: 49px;
